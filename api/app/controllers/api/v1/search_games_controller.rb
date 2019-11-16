9032edff266a5643d1853b62a75b147d638abd64
oliveira-andre <desmonplays@gmail.com>
2019-11-16 18:40:04 -0400

    (new feature) execute the job

--- api/app/controllers/api/v1/search_games_controller.rb
+++ api/app/controllers/api/v1/search_games_controller.rb
@@ -4,15 +4,20 @@ module Api
   module V1
     class SearchGamesController < ApplicationController
       before_action :load_nuuvem_games
+      before_action :send_message
 
       def index
-        render json: { nuuvem_games: @nuuvem_games }
+        render json: { games: @nuuvem_games }
       end
 
       private
 
       def load_nuuvem_games
         @nuuvem_games = NuuvemSearchService.execute
+      end
+
+      def send_message
+        LaunchTelegramBotJob.perform_later(@nuuvem_games)
       end
     end
   end
