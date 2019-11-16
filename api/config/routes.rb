2c8790249d7f1b27198e5bf19a1a203ab7e85ab3
oliveira-andre <desmonplays@gmail.com>
2019-11-16 18:39:32 -0400

    (new feature) mounting sidekiq url

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   namespace :api do
     namespace :v1 do
       resources :search_games, only: :index
